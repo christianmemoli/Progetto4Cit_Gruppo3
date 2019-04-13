459ccf8099180cc35d72d10b7480360aabeea144
MatteoCapella01 <matteo.capella2001@gmail.com>
2019-04-13 12:19:41 +0200

    codice piu grafica

---
+++ Codice/Grafica/src/application/Main.java
@@ -0,0 +1,27 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+		Parent rootNode = FXMLLoader.load(getClass().getResource("prova.fxml"));
+		Scene homeScene = new Scene(rootNode, 300, 250);
+		primaryStage.setScene(homeScene);
+		primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
