0b326000b22a0eeb28c32d4297c158a0706e6a5b
Luigi Zucconi <luigizucconi1@gmail.com>
2019-05-29 09:54:34 +0200

    Classe pronatazioni e aggiunta controllo delle prenotazione nella classe Test

--- Grafica/src/application/Prenotazione.java
+++ Grafica/src/application/Prenotazione.java
@@ -3,14 +3,20 @@ package application;
 public class Prenotazione {
 	private int n_bagagli;
 	private double peso_complessivo;
+	
+	private String nome;
+	private String codice_prenotazione;
+	
 	public Prenotazione()
 	{
 		
 	}
-	public Prenotazione(int n_bagagli, double peso_complessivo)
+	public Prenotazione(int n_bagagli, double peso_complessivo, String nome, String codice_prenotazione)
 	{
 		this.n_bagagli=n_bagagli;
 		this.peso_complessivo=peso_complessivo;
+		this.nome = nome;
+		this.codice_prenotazione = codice_prenotazione;
 	}
 	public Prenotazione(Prenotazione p)
 	{
@@ -29,8 +35,27 @@ public class Prenotazione {
 	public void setPeso_complessivo(double peso_complessivo) {
 		this.peso_complessivo = peso_complessivo;
 	}
+	
+	public String getNome(){
+		
+		return nome;
+	}
+	public void setNome(String nome){
+		
+		this.nome = nome;
+	}
+	
+	public String getCodice_prenotazione(){
+		
+		return codice_prenotazione;
+	}
+	public void setCodice_prenotazione(String codice_prenotazione){
+		
+		this.codice_prenotazione = codice_prenotazione;
+	}
+	
 	public String toString()
 	{
-		return "Numero dei bagagli: "+n_bagagli+"\nPeso complessivo dei bagagli: "+peso_complessivo;
+		return "Nome prenotazione: " + nome + "\nCodice Prenotazione: " + codice_prenotazione + "\nNumero dei bagagli: " + n_bagagli + "\nPeso complessivo dei bagagli: " +peso_complessivo;
 	}
 }
--- Grafica/src/application/Test.java
+++ Grafica/src/application/Test.java
@@ -5,12 +5,18 @@ public class Test {
 
 	public static void main(String[] args) throws SomethingException {
 		// TODO Auto-generated method stub
-		Cliente c1=new Cliente();
-		Volo v1=new Volo();
-		Clienti c=new Clienti();
-		Voli v=new Voli();
-		int scelta=0;
+		
+		Cliente c1 = new Cliente();
+		Volo v1 = new Volo();
+		Clienti c = new Clienti();
+		Voli v = new Voli();
+		Prenotazione p1 = new Prenotazione();
+		Prenotazioni p = new Prenotazioni();
+		
+		int scelta = 0;
+		
 		Scanner s1=new Scanner(System.in);
+		
 		do
 		{
 			System.out.println("Benvenuti nel menù a tendina della nostra agenzia di volo. Eseguire una scelta:");
@@ -18,7 +24,9 @@ public class Test {
 			System.out.println("2-Per volo;");
 			System.out.println("3-Per prenotazione.");
 			System.out.println("(PREMERE 4 PER USCIRE DAL MENU)");
+			
 			scelta=s1.nextInt();
+			
 			switch(scelta)
 			{
 			case 1:
@@ -33,10 +41,13 @@ public class Test {
 					System.out.println("4-Cerca cliente a partire dal nome e cognome;");
 					System.out.println("5-Modifica dati del cliente a partire dal codice;");
 					System.out.println("(PREMERE 6 PER USCIRE DAL MENU)");
+					
 					scelta2=s2.nextInt();
+					
 					switch(scelta2)
 					{
 					case 1:
+						
 						Scanner s3=new Scanner(System.in);
 						System.out.print("Nome: ");
 						c1.setNome(s3.nextLine());
@@ -50,6 +61,7 @@ public class Test {
 						c1.setData_nascita(s3.nextLine());
 						System.out.print("Codice clietne: ");
 						c1.setCodice_cliente(s3.nextLine());
+						
 						try
 						{
 							c.addCliente(c1);
@@ -58,7 +70,9 @@ public class Test {
 							System.out.println(s);
 						}
 						break;
+						
 					case 2:
+						
 						Scanner s4=new Scanner(System.in);
 						System.out.print("Inserire Il codice del cliente da eliminare: ");
 						try
@@ -175,10 +189,13 @@ public class Test {
 				break;
 				
 				// DA CORREGGERE (PRENOTAZIONE)
-				// CAMBIA TUTTO CON VARIABILI E METODI DI PRENOTAZIONI
+				
 			case 3:
+				
 				int scelta4=0;
+				
 				Scanner s4=new Scanner(System.in);
+				
 				do
 				{
 					System.out.println("Scegliere l'attività:");
@@ -188,66 +205,68 @@ public class Test {
 					System.out.println("(PREMERE 4 PER USCIRE DAL MENU)");
 					
 					int scelta5;
-					Scanner s5=new Scanner(System.in);
-					scelta5 =s5.nextInt();
+					
+					Scanner s5 = new Scanner(System.in);
+					
+					scelta5 = s5.nextInt();
 					
 					switch(scelta5)
 					{
 					case 1:
-						Scanner s3=new Scanner(System.in);
+						Scanner s3 = new Scanner(System.in);
 						System.out.print("Nome: ");
-						c1.setNome(s3.nextLine());
+						p1.setNome(s3.nextLine());
 						System.out.print("Codice prenotazione: "); // LO GENRIAMO AUTOMATICAMENTE?
-						c1.setCodice_cliente(s3.nextLine());
+						p1.setCodice_prenotazione(s3.nextLine());
+						
 						try
 						{
-							c.addCliente(c1);
+							p.addPrenotazione(p1);
+							
 						}catch(SomethingException s)
 						{
 							System.out.println(s);
 						}
 						break;
+						
 					case 2:
-						Scanner s6=new Scanner(System.in);
-						System.out.print("Inserire Il codice del cliente da eliminare: ");
+						
+						Scanner s6 = new Scanner(System.in);
+						
+						System.out.print("Inserire Il codice della prenotazione da eliminare: ");
+						
 						try
 						{
-							c.eliminaCliente(s5.nextLine());
+							p.eliminaPrenotazione(s6.nextLine());
 						}catch(SomethingException s)
 						{
 							System.out.println(s);
 						}
 						break;
+						
 					case 3:
+						
 						Scanner s8=new Scanner(System.in);
-						System.out.print("Inserire Il codice del cliente da cercare: ");
+						
+						System.out.print("Inserire il nome della prenotazione da cercare: ");
+						
 						try
 						{
-							System.out.println(c.cercaClienteCodice(s8.nextLine()));
+							System.out.println(p.cercaPrenotazioneNome(s8.nextLine()));
+							
 						}catch(SomethingException s)
 						{
 							System.out.println(s);
 						}
 						break;
+						
 					case 4:
-						Scanner s9=new Scanner(System.in);
-						System.out.print("Inserire Il nome e il cognome del cliente da cercare: ");
-						try
-						{
-							System.out.println(c.cercaClienteNomeCognome(s9.nextLine(), s9.nextLine()));
-						}catch(SomethingException s)
-						{
-							System.out.println(s);
-						}
-						break;
-					case 5:
 						
 					}
-				}while(scelta4<=5);
+				}while(scelta4<=4);
 				break;
 			}
 		}while(scelta<=3);
 		System.out.println("Grazie per aver scelto la nostra compagnia.");
 	}
-
 }
