f3b39c6375d50c2c4a6dd6b2deb6cbbc2cac2e60
Luigi Zucconi <luigizucconi1@gmail.com>
2019-05-30 12:02:17 +0200

    aggiornamento prenot

--- Grafica/src/application/Prenotazione.java
+++ Grafica/src/application/Prenotazione.java
@@ -3,34 +3,50 @@ package application;
 public class Prenotazione {
 	private int n_bagagli;
 	private double peso_complessivo;
+
+	private String nome;
+	private String codice_prenotazione;
+
 	public Prenotazione()
 	{
-		
+
 	}
-	public Prenotazione(int n_bagagli, double peso_complessivo)
+	public Prenotazione(int n_bagagli, double peso_complessivo, String nome, String codice_prenotazione)
 	{
 		this.n_bagagli=n_bagagli;
 		this.peso_complessivo=peso_complessivo;
+		this.nome = nome;
+		this.codice_prenotazione = codice_prenotazione;
 	}
 	public Prenotazione(Prenotazione p)
 	{
-		this.n_bagagli=p.n_bagagli;
-		this.peso_complessivo=p.peso_complessivo;
+		
 	}
-	public int getN_bagagli() {
-		return n_bagagli;
-	}
-	public void setN_bagagli(int n_bagagli) {
-		this.n_bagagli = n_bagagli;
-	}
-	public double getPeso_complessivo() {
-		return peso_complessivo;
-	}
-	public void setPeso_complessivo(double peso_complessivo) {
-		this.peso_complessivo = peso_complessivo;
-	}
-	public String toString()
-	{
-		return "Numero dei bagagli: "+n_bagagli+"\nPeso complessivo dei bagagli: "+peso_complessivo;
-	}
+		public void setPeso_complessivo(double peso_complessivo) {
+			this.peso_complessivo = peso_complessivo;
+		}
+
+		public String getNome(){
+
+			return nome;
+		}
+		public void setNome(String nome){
+
+			this.nome = nome;
+		}
+
+		public String getCodice_prenotazione(){
+
+			return codice_prenotazione;
+		}
+		public void setCodice_prenotazione(String codice_prenotazione){
+
+			this.codice_prenotazione = codice_prenotazione;
+		}
+
+		public String toString()
+		{
+			return "Nome prenotazione: " + nome + "\nCodice Prenotazione: " + codice_prenotazione + "\nNumero dei bagagli: " + n_bagagli + "\nPeso complessivo dei bagagli: " +peso_complessivo;
+		}
+		
 }
--- Grafica/src/application/Prenotazioni.java
+++ Grafica/src/application/Prenotazioni.java
@@ -1,5 +1,48 @@
 package application;
-public class Prenotazioni {
 
-	
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Prenotazioni extends SomethingException{ //ESTENDE ECCEZZIONE?
+	public final static int NUM_MAX_P = 1000000;
+	private ArrayList<Prenotazione>p;
+
+	public Prenotazioni()
+	{
+		p = new ArrayList<>(NUM_MAX_P);
+	}
+
+	public void addPrenotazione(Prenotazione prenotazione) throws SomethingException
+	{
+		if(p.size()>=NUM_MAX_P)
+		{
+			throw new SomethingException();
+		}
+		p.add(prenotazione);
+	}
+	public void eliminaPrenotazione(String prenotazione) throws SomethingException
+	{
+		Iterator<Prenotazione>i;
+		for(i=p.iterator(); i.hasNext();)
+		{
+			Prenotazione prenot = i.next();
+			if(prenot.getCodice_prenotazione().equals(prenotazione))
+			{
+				i.remove();
+				return;
+			}
+		}
+		throw new SomethingException();
+	}
+	public Prenotazione cercaPrenotazioneNome(String nome) throws SomethingException 
+	{
+		for(Prenotazione prenotazione:p)
+		{
+			if(prenotazione.getNome().equals(nome))
+			{
+				return prenotazione;
+			}
+		}
+		return null;
+	}
 }
